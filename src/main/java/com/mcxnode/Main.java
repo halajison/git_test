22fb708898240d18c6a749221dc17a55c2c34ed5
halajison <1831091946@qq.com>
2023-01-28 13:29:48 +0800

    first commit

---
+++ src/main/java/com/mcxnode/Main.java
@@ -0,0 +1,12 @@
+package com.mcxnode;
+
+/**
+ * @description:
+ * @anthor halajison
+ * @create ${YEAR}-${MONTH}-${DAY}-${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
