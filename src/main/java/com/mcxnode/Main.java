6f20c786ab4c6379a0731b0cec6f3ae8cb6c9bb5
halajison <78647218+halajison@users.noreply.github.com>
2023-01-28 13:52:49 +0800

    Update Main.java

--- src/main/java/com/mcxnode/Main.java
+++ src/main/java/com/mcxnode/Main.java
@@ -9,5 +9,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world12122!");
         System.out.println("Hello world12122!");
+        System.out.println("Hello world12122!");
+        System.out.println("Hello world12122!");
+
     }
 }
